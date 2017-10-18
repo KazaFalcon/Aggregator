0a3c627d97a389373294fb78d11c004c4709ce2c
KazaFalcon <sameersaxena@dynaptsolutions.com>
2017-10-18 18:31:26 +0530

    Delete vehicleStop.sh

--- vehicleStop.sh
+++
@@ -1,8 +0,0 @@
-#!bib/bash
-cd /sensorsApp/gpsdata/
-startFile=$(find -name '*.sdt' | sort -V | head -1)
-endFile=$(find -name '*.sdt' | sort -V | tail -1)
-#echo $startFile
-#echo $endFile
-python /home/project/calculateDistance.py $startFile $endFile
-
