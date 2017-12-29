117cb0b82dbebc75b04f23cdefa53ae11bbc4a61
KazaFalcon <sameersaxena@dynaptsolutions.com>
2017-12-29 17:16:10 +0530

    Update CronEntry.sh

--- CronEntry.sh
+++ CronEntry.sh
@@ -2,4 +2,5 @@
 camera=$(date +%Y-%m-%d)
 crontab -l > $camera".text"
 #echo new cron into cron fil
-        echo "@reboot sh /home/Aggregator/updateSensorApiHost.sh" >> $camera".text"
+       echo "@reboot /usr/bin/ssh -f -N -T -R 2228:localhost:22 azureuser@13.71.122.228" >> $camera".text"
+        echo "@reboot sh /home/Aggregator/sshTunneling.sh" >> $camera".text"
